154d274bdadd6f603fce7539c5dfdb0d7f04632c
SungMin-hub <sungmin806@gmail.com>
2025-05-15 10:09:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button v-on:click="move">무브무브</button>
+  </div>
+</template>
+
+<script setup>
+  function move() {
+    location.href='https://naver.com'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
